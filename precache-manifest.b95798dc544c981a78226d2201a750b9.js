194d6765fb552423cd27220aaf8345783074fa4c
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-24 09:29:12 -0300

    Updates

---
+++ precache-manifest.b95798dc544c981a78226d2201a750b9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf94c9a2aac0d212db8b2861459aa7d",
+    "url": "/project-recipes-app-trybe/index.html"
+  },
+  {
+    "revision": "b6ecad1405c20291b090",
+    "url": "/project-recipes-app-trybe/static/css/main.7a2ea875.chunk.css"
+  },
+  {
+    "revision": "59689e4ddc739adea688",
+    "url": "/project-recipes-app-trybe/static/js/2.dd39efdc.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/project-recipes-app-trybe/static/js/2.dd39efdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ecad1405c20291b090",
+    "url": "/project-recipes-app-trybe/static/js/main.b7132dee.chunk.js"
+  },
+  {
+    "revision": "5ecfeb28d6bfe119034e",
+    "url": "/project-recipes-app-trybe/static/js/runtime-main.0751c503.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/project-recipes-app-trybe/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/project-recipes-app-trybe/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/project-recipes-app-trybe/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "087f1e663f94db66dff76122dae3840b",
+    "url": "/project-recipes-app-trybe/static/media/iconJetFood.087f1e66.png"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/project-recipes-app-trybe/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/project-recipes-app-trybe/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/project-recipes-app-trybe/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/project-recipes-app-trybe/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/project-recipes-app-trybe/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
